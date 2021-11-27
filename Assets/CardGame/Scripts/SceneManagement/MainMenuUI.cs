a8ce4f061a75340bc6aa380dad29c0a5a9dee13d
Ben James <39443778+Benjames01@users.noreply.github.com>
2021-11-27 19:50:09 +0000

    Add SceneLoader and MainMenuUI to assist Scene Transitions

---
+++ Assets/CardGame/Scripts/SceneManagement/MainMenuUI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+
+  public void OnButtonClick(int i)
+  {
+    switch (i)
+    {
+      case 1:
+        SceneLoader.LoadScene(SceneLoader.Scene.GameScene);
+        break;
+      case 2:
+        SceneLoader.LoadScene(SceneLoader.Scene.CardCreator);
+        break;
+      case 3:
+        SceneLoader.LoadScene(SceneLoader.Scene.SettingsScene);
+        break;
+      case 4:
+        Application.Quit();
+        break;
+    }
+  }
+    
+    
+}
